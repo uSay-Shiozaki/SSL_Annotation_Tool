536622670cbe62c6820999bd72adbecd6cad2680
USay <csys21001@g.nihon-u.ac.jp>
2023-07-19 09:15:03 +0900

    add REST API functions

--- app/src/Lib/widgets/MainScreen.py
+++ app/src/Lib/widgets/MainScreen.py
@@ -11,37 +11,3 @@ class MainScreen(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         print("Main Screen Launched")
-
-    def swavModuleExecution(self):
-        boolSemi = False
-        boolCreateDataset = False
-
-        if not boolSemi:
-            try:
-                os.system(
-                    "python -m torch.distributed.launch --nproc_per_node=1 \
-                        ./ml_models/mycodes/app_modules/swav_modules/swav_semisup_module.py"
-                )
-            except:
-                print("semi-learning failed")
-                return
-            boolSemi = True
-
-        else:
-            try:
-                os.system(
-                    "python -m torch.distributed.launch --nproc_per_node=1 \
-                        ./ml_models/mycodes/app_modules/swav_modules/swav_classification_module.py"
-                )
-            except:
-                print("classification failed")
-
-    def ibotModuleExecution(self):
-        try:
-            os.system(
-                "python -m torch.distributed.launch --nproc_per_node=1 \
-                ./mypackages/ibot_unsup_module.py"
-            )
-        except:
-            print("Clustering Failed")
-            return
