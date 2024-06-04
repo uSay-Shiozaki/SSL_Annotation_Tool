5870db291d96e4d421ab3445ceef8f0ffc22f2df
USay <csys21001@g.nihon-u.ac.jp>
2024-06-04 18:02:41 +0900

    changed install.sh and fix Dockerfile on server

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
-python3 -m pip install "kivy[full]" kivymd
+# if you want install kivy on your root environemnts
+# python3 -m pip install "kivy[full]" kivymd
+
+# install gdown to download file from my public drive.
 apt get install gdown
 
 # Pretrained-weight from CIFAR10 for iBOT
 gdown https://drive.google.com/uc?id=1nJwAcx8pDRTHo3eeyGz520DkY_dVP24G
+
 # Pretrained-weight from ImageNet for iBOT
 # gdown https://drive.google.com/uc?id=1BSnxAYuAAumpK9pgTRnKoC6UJGeDs_GC
+
 # CIFAR 10 Image Dataset
 gdown https://drive.google.com/uc?id=1rdCZwn5jMQbKurscJTjM8jz3WkFgSlEc
+
 # pretrained-weight from ImageNet for SwAV
 # gdown https://drive.google.com/uc?id=15_K21qGvUvEqbkZKu0NHT2VyOtFgn5RR
