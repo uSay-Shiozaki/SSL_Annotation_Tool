b53155a3de77f7639dd5d3f4a3ab271fc6fea22c
USay <csys21001@g.nihon-u.ac.jp>
2024-06-04 17:42:53 +0900

    rm RUN gdown command and add them to install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
 python3 -m pip install "kivy[full]" kivymd
+apt get install gdown
+
+# CIFAR10
+gdown https://drive.google.com/uc?id=1nJwAcx8pDRTHo3eeyGz520DkY_dVP24G
+# ImageNet
+gdown httos://drive.google.com/uc?id=1BSnxAYuAAumpK9pgTRnKoC6UJGeDs_GC
+# Pretrained-weight from CIFAR10
+gdown httos://drive.google.com/uc?id=15_K21qGvUvEqbkZKu0NHT2VyOtFgn5RR
